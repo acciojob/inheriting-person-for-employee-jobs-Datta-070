45a8f6578d1257867de4a85b6106546c22878beb
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-12-21 11:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function Employee(name, age, jobTitle) {
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 Employee.prototype.jobGreet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 // Do not change code below this line
